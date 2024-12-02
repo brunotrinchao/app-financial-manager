b8bef699ea90e66b6b8f140a793688d485ede3fb
brunotrinchao <bruno.souza@onfly.com.br>
2024-12-02 10:44:05 -0300

    upd url e env

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,17 +1,17 @@
 let BASE, BASE_API, BASE_IMG, SERVER, ENVIRONMENT;
-if (process.env.VUE_APP_DEVELOPMENT == 'development') {
-  BASE = process.env.VUE_APP_API_URL;
-  BASE_API = `${BASE}/api`;
-  BASE_IMG = `${BASE}`;
-  SERVER = 'D';
-  ENVIRONMENT = process.env.VUE_APP_DEVELOPMENT;
-} else {
-  BASE = process.env.VUE_APP_API_URL;
-  BASE_API = `${BASE}/api`;
-  BASE_IMG = `${BASE}`;
-  SERVER = 'P';
-  ENVIRONMENT = process.env.VUE_APP_DEVELOPMENT;
-}
+// if (process.env.VUE_APP_DEVELOPMENT == 'development') {
+//   BASE = process.env.VUE_APP_API_URL;
+//   BASE_API = `${BASE}/api`;
+//   BASE_IMG = `${BASE}`;
+//   SERVER = 'D';
+//   ENVIRONMENT = process.env.VUE_APP_DEVELOPMENT;
+// } else {
+BASE = process.env.VUE_APP_API_URL;
+BASE_API = `${BASE}/api`;
+BASE_IMG = `${BASE}`;
+SERVER = 'P';
+ENVIRONMENT = process.env.VUE_APP_DEVELOPMENT;
+// }
 
 export default {
   BASE_API: BASE_API,
